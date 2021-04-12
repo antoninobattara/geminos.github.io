f7c5cc7d6305424de781c8a1b54beca3b53b3948
antoninobattara <82031474+antoninobattara@users.noreply.github.com>
2021-04-12 14:40:01 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,12 @@
+<script>
+	$(document).ready(function(){
+	
+	
+		$(".header__icona").click(function(e){
+		
+			$(".header__menu").toggleClass('is-open');
+			e.preventDefault();
+		}
+		
+	});
+</script>
